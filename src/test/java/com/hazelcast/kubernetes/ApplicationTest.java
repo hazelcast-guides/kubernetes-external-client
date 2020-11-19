dfd44d290fe383efb6b39c17029f7e5079eba468
Serge Bishyr <seriybg@gmail.com>
2020-11-19 12:15:31 +0200

    change cluster name to be unique

--- src/test/java/com/hazelcast/kubernetes/ApplicationTest.java
+++ src/test/java/com/hazelcast/kubernetes/ApplicationTest.java
@@ -7,12 +7,6 @@ import com.hazelcast.config.KubernetesConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 
